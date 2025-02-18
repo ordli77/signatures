6e74392c373d2960fa6a64159c64ab8871ec9db9
ordli77 <gerardlee58@gmail.com>
2025-02-18 11:12:39 +0100

    initial commit

--- src/get_cost.py
+++ src/get_cost.py
@@ -12,10 +12,9 @@ import matplotlib.pyplot as plt
 
 
 
-def get_costs(params,Sigma):
-    print('Enter the type of process, BM for brownian motion, FBM for fractional Brownian motion')
+def get_costs(params,Sigma,name):
     
-    name =input()
+    
     if name =="BM":#generate the midprice process
         process = BM(sigma=Sigma)
     else:
@@ -26,16 +25,16 @@ def get_costs(params,Sigma):
     ############################################
     #save the price paths into a parameter specified txt file                  
     #try:
-    with open("midprice of n={},H={} and order{}.txt".format(params["n_step"],params["H"],params["order"],Sigma), "w") as txt_file:
+    with open("C:\\Users\\gerar\\project1\\data\\midprice of n={},H={} and order{},sigma{}.txt".format(params["n_step"],params["H"],params["order"],Sigma), "w") as txt_file:
         for line in paths:
             txt_file.write("{}".format(params["order"]) + " ".join(map(str, line) )+ "\n")
     #except FileExistsError:
        #print(f&quot;The file "midprice of n={},H={} and order{}.txt".format(params["n_step"],params["H"],params["order"]) already exists.&quot;)
     ############################################
     #run the optimization algorithm, obtain the l as vector of coefficients
-
+    params["sigma"]=Sigma
     l=optimise(ES,**params) 
-    with open("coeffecient{}.txt".format(Sigma), "w") as f:
+    with open("C:\\Users\\gerar\\project1\\data\\coeffecient{}.txt".format(Sigma), "w") as f:
     
        f.write(name +"{}".format(l))
     ############################################
@@ -46,21 +45,23 @@ def get_costs(params,Sigma):
     ############################################
      #evaluate the model using the testing sets, and plot the terminal wealth distribution,the dynamic of the inventory, and trading speeds
     
-    speeds, inventories, wealths = utils.get_analytics(utils.sig_speed(l,params["N"]),samples,**params)
-                      
-    plot(wealths,inventories,speeds)                
+    speeds, inventories, wealths = utils.get_analytics(utils.sig_speed(l,params["N"]),paths,**params)
+ 
+    #plot(wealths,inventories,speeds)                
     
     
     ##############################################################
+    #compute the terminal wealth numerically, and estimate the second stage monte carlo error 
 
-    costs = performance.cost(samples,l,**params)
+    costs = performance.cost(paths,l,**params)
 
     mean = np.mean(costs)
-  
-    size=params["sample_size"]
-    confidence_interval=utils.confidence_interval(mean,size,0.05,std)
-    print(mean,np.mean(confidence_interval))
-    with open("results of steps {}, Hurst {},{} and order {}.txt".format(params["n_step"],params["H"],Sigma,params["order"]), "w") as file:
+    stds = np.std(costs)
+    sample_size=params["n_paths"]
+    confidence_interval=utils.confidence_interval(mean,sample_size,0.05,stds)
+
+    with open("C:\\Users\\gerar\\project1\\data\\results of steps_test {}, Hurst {},{} and order {}.txt".format(params["n_step"],params["H"],Sigma,params["order"]), "w") as file:
     
-       file.write("terminal wealth {} Monte carlo error {}".format(mean,np.mean(confidence_interval)))
-    
+       file.write("terminal wealth_test {} Monte carlo error {}".format(mean,np.mean(confidence_interval)))
+    return(mean, np.mean(confidence_interval))
+
--- src/loop_sigma.py
+++ src/loop_sigma.py
@@ -1,4 +1,6 @@
 import numpy as np
+from statsmodels.graphics.tukeyplot import results
+
 import utils
 from optimise import optimise
 from process.bm import BM
@@ -9,14 +11,14 @@ import performance
 import matplotlib.pyplot as plt
 from get_cost import get_costs
 
-sigmas = np.array([0.02,0.03,0.04,0.05,0.06,0.08])
+sigmas = np.array([0.08])
 
 params = {
     "n_step":100,
-    "n_paths":50000,
-    "N":3,
+    "n_paths":20000,
+    "N":2,
     "H":0.7,
-    "order": 9,
+    "order": 7,
     "q_0": 1.,
     "k":  1e-4,
     "lambd": 1e-3,
@@ -24,6 +26,12 @@ params = {
     "phi": 0,
     "sample_size" :10000
 }
-
+print('Enter the type of process, BM for brownian motion, FBM for fractional Brownian motion')
+name =input()
+    
+result =np.array([])
 for sig in sigmas:
-    get_costs(params,sig)
+    cost=get_costs(params,sig,name)
+    np.append(result,cost[0])
+
+print(result)
